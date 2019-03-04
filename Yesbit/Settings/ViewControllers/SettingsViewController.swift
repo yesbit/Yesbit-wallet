4e78a42aa9a720e1421ce0d88bfbb0e7db9d16af
Kangcheng Ji <kan.ji99@yahoo.com>
2019-03-04 14:02:27 -0500

    started working on push notifications

--- Yesbit/Settings/ViewControllers/SettingsViewController.swift
+++ Yesbit/Settings/ViewControllers/SettingsViewController.swift
@@ -97,23 +97,23 @@ final class SettingsViewController: FormViewController, Coordinator {
 //                cell.imageView?.image = R.image.settings_colorful_security()
 //            }
 
-//            <<< autoLockRow
-//
-//            <<< AppFormAppearance.button { [weak self] row in
-//                row.cellStyle = .value1
-//                row.presentationMode = .show(controllerProvider: ControllerProvider<UIViewController>.callback {
-//                    let controller = NotificationsViewController()
-//                    controller.didChange = { [weak self] change in
-//                        self?.run(action: .pushNotifications(change))
-//                    }
-//                    return controller
-//                }, onDismiss: { _ in
-//            })
-//            }.cellUpdate { cell, _ in
-//                cell.imageView?.image = R.image.settings_colorful_notifications()
-//                cell.textLabel?.text = R.string.localizable.settingsPushNotificationsTitle()
-//                cell.accessoryType = .disclosureIndicator
-//            }
+            <<< autoLockRow
+
+            <<< AppFormAppearance.button { [weak self] row in
+                row.cellStyle = .value1
+                row.presentationMode = .show(controllerProvider: ControllerProvider<UIViewController>.callback {
+                    let controller = NotificationsViewController()
+                    controller.didChange = { [weak self] change in
+                        self?.run(action: .pushNotifications(change))
+                    }
+                    return controller
+                }, onDismiss: { _ in
+            })
+            }.cellUpdate { cell, _ in
+                cell.imageView?.image = R.image.settings_colorful_notifications()
+                cell.textLabel?.text = R.string.localizable.settingsPushNotificationsTitle()
+                cell.accessoryType = .disclosureIndicator
+            }
 
             +++ Section()
 
